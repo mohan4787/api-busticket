42726a205192bc5b2892a7423bacea5335f72a04
mohan4787 <bishwoshah.it@gmail.com>
2025-10-01 17:52:21 +0545

    update middleware

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,93 @@
+class AuthController {
+  registerUser = (req, res, next) => {
+    const data = req.body;
+    res.json({
+      data: data,
+      message: "register",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  activateUser = (req, res, next) => {
+    let params = req.params;
+    const headers = req.headers;
+    const query = req.query;
+
+    res.json({
+      data: {
+        params,
+        headers,
+        query,
+      },
+      message: "activate token",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  loginUser = (req, res, next) => {
+    res.json({
+      data: null,
+      message: "login",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  forgetPasswordRequest = (req, res, next) => {
+    res.json({
+      data: null,
+      message: "forget password",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  forgetPasswordTokenVerify = (req, res, next) => {
+    res.json({
+      data: req.params.token,
+      message: "forget password verify",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  resetPassword = (req, res, next) => {
+    res.json({
+      data: null,
+      message: "reset password",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  loggedInUserProfile = (req, res, next) => {
+    res.json({
+      data: null,
+      message: "me",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  logoutUser = (req, res, next) => {
+    res.json({
+      data: null,
+      message: "logout",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+
+  updateUserById = (req, res, next) => {
+    res.json({
+      data: req.params.id,
+      message: "update user",
+      status: "SUCCESS",
+      options: null,
+    });
+  };
+}
+
+module.exports = AuthController;
---
+++ src/modules/auth/auth.router.js
@@ -0,0 +1,29 @@
+const authRouter = require("express").Router()
+const bodyValidator = require("../../middlewares/request-validate.middleware")
+const AuthController = require("./auth.controller")
+const { RegisterDTO, LoginDTO, ResetPasswordRequestDTO } = require("./auth.validators")
+
+const authCtrl = new AuthController()
+
+
+authRouter.post("/register", bodyValidator(RegisterDTO), authCtrl.registerUser)
+
+authRouter.get("/activate/:token", authCtrl.activateUser)
+
+authRouter.post("/login",bodyValidator(LoginDTO), authCtrl.loginUser)
+
+authRouter.post("/forget-password",bodyValidator(ResetPasswordRequestDTO) ,authCtrl.forgetPasswordRequest)
+
+authRouter.get("/forget-password-verify/:token", authCtrl.forgetPasswordTokenVerify)
+
+authRouter.put("/reset-password", authCtrl.resetPassword)
+
+authRouter.get("/me", authCtrl.loggedInUserProfile)
+
+authRouter.get("/logout", authCtrl.logoutUser)
+
+authRouter.put("/user/:id", authCtrl.updateUserById)
+
+
+
+module.exports = authRouter;
